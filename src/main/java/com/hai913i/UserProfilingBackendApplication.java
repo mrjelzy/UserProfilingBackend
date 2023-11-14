dc395d7b1ad468a9908d123d8419fad3ff2cd13f
Ryan Bengoufa <ryan.bengoufa@etu.umontpellier.fr>
2023-11-14 09:08:16 +0100

    init

---
+++ src/main/java/com/hai913i/UserProfilingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hai913i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfilingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfilingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hai913i/UserProfilingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hai913i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfilingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
